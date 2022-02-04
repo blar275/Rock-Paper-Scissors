7be8ba466fd2bae8b6d2e862255f7ddf4d279e0d
blar275 <coolestarr@gmail.com>
2022-02-03 23:04:33 -0500

    created working buttons

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const rounds = 5
+//const rounds = 5
+let playerScore = 0
+let rounds = 0
 
 function computerPlay() {
     let rand = Math.trunc(Math.random() * 3)
@@ -21,20 +23,14 @@ function playGame(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-
-    for (let i=0; i<rounds; i++) {
-        let playerSelection = window.prompt("Enter Rock, Paper, or Scissors")
-        let computerSelection = computerPlay()
-        let outcome = playGame(playerSelection, computerSelection)
-        console.log(outcome[0])
-
-        if (outcome[1] === 2) {i--}
-        else {playerScore = playerScore + outcome[1]}
-    }
-
-    if (playerScore < 3) return(`You lose! player: ${playerScore} computer: ${rounds-playerScore}`)
+function makeSelection(selection) {
+    let result = playGame(selection, computerPlay())
+    console.log(selection)
+    if(result[1] !== 2) updateScoreboard()
 }
 
-console.log(game())
+function reset() {
+    playerScore = 0
+    rounds = 0
+    updateScoreboard()
+}
